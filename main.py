8c93e3e18a0f0a331d854724996f7160d2125ec8
Rusthen Ronald <rusthen.machado@sergipetec.org.br>
2025-03-28 20:50:27 -0300

    retornando listas

--- main.py
+++ main.py
@@ -3,6 +3,13 @@ from fastapi import FastAPI
 app=FastAPI()
 
 
+vendas={
+    1:{"item":"lata","preço_unitário":4,"quantidade":5},
+    2:{"item":"lata","preço_unitário":4,"quantidade":5},
+    3:{"item":"lata","preço_unitário":4,"quantidade":5},
+    4:{"item":"lata","preço_unitário":4,"quantidade":5}
+}
+
 @app.get('/')
 def home ():
-    return "Hello world"
+    return {"vendas":len(vendas)}
