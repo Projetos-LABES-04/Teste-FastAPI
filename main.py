6f4df540a3b4ef7ffd963f1f6e9ea191ae60eaa1
Rusthen Ronald <rusthen.machado@sergipetec.org.br>
2025-03-31 23:50:11 -0300

    criando pasta routers para organizar endpoints

--- main.py
+++ main.py
@@ -2,14 +2,6 @@ from fastapi import FastAPI
 
 app=FastAPI()
 
-
-vendas={
-    1:{"item":"lata","preço_unitário":4,"quantidade":5},
-    2:{"item":"lata","preço_unitário":4,"quantidade":5},
-    3:{"item":"lata","preço_unitário":4,"quantidade":5},
-    4:{"item":"lata","preço_unitário":4,"quantidade":5}
-}
-
 @app.get('/')
 def home ():
-    return {"vendas":len(vendas)}
+    return 
