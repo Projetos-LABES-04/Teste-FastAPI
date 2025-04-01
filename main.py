f533d1cfffef6e223335ff17a1267ebc0daa09da
Rusthen Ronald <rusthen.machado@sergipetec.org.br>
2025-03-31 23:57:56 -0300

    adicionando rotas

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ from routers import auth,dashboard,transacoes ,contas,notificacoes
 
 app=FastAPI()
 
-@app.get('/')
-def home ():
-    return "Hello World"
+app.include_router(auth.router, prefix="/auth", tags=["Autenticação"])
+app.include_router(dashboard.router, prefix="/dashboard", tags=["Dashboard"])
+app.include_router(transacoes.router, prefix="/transacoes", tags=["Transações"])
+app.include_router(contas.router, prefix="/contas", tags=["Contas"])
