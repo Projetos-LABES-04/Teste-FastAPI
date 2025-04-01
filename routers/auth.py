ec44a634f9b2c5e1853b72f22721f4fdd1e0f9b5
Rusthen Ronald <rusthen.machado@sergipetec.org.br>
2025-04-01 00:11:34 -0300

    verficando se credenciais estão corretas

--- routers/auth.py
+++ routers/auth.py
@@ -11,3 +11,9 @@ class LoginRequest(BaseModel):
 class TokenResponse(BaseModel):
     access_token: str
     token_type: str = "bearer"
+
+
+@router.post("/login", response_model=TokenResponse)
+async def login(login_data: LoginRequest):
+    if login_data.username == "admin" and login_data.password == "senha123":
+        return {"access_token": "token_exemplo"}
