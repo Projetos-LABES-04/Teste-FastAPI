2014dab7136ac38345deaa23b3b2e52bb2f78de6
Rusthen Ronald <rusthen.machado@sergipetec.org.br>
2025-04-01 00:26:32 -0300

    definindo estrutura de uma transação

--- routers/transacoes.py
+++ routers/transacoes.py
@@ -2,3 +2,12 @@ from fastapi import APIRouter, HTTPException
 from pydantic import BaseModel
 from typing import List, Optional
 
+router = APIRouter()
+
+class Transacao(BaseModel):
+    id: int
+    conta: str
+    data: str
+    valor: float
+    localizacao: str
+    status: str  # normal, suspeita, em análise
