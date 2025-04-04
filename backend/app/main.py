b5952883770a9693f3cc1443a0daba1f60605da9
Rusthen Ronald <rusthen.machado@sergipetec.org.br>
2025-04-04 15:42:16 -0300

    desinstalando venv

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,2 +1,15 @@
  # Ponto de entrada da API
+from fastapi import FastAPI
 from routes import transacao_routes, conta_routes, notificacao_routes, dashboard_routes, auth_routes
+
+
+app=FastAPI()
+
+#Adiciona essas rotas à sua aplicação principal
+
+app.include_router(transacao_routes.router)
+app.include_router(conta_routes.router)
+app.include_router(notificacao_routes.router)
+app.include_router(dashboard_routes.router)
+app.include_router(auth_routes.router)
+
